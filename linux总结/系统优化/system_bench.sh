fac00486740db26a894b4cbe0393a40b750d7ac2
huruizhi <huruizhi@pystandard.com>
2018-12-26 17:59:44 +0800

    sysbench 脚本

--- linux总结/系统优化/system_bench.sh
+++ linux总结/系统优化/system_bench.sh
@@ -50,13 +50,13 @@ io_test(){
 	sysbench fileio --file-total-size=150G --file-num=20 prepare
 	echo -e "\033[32m环境准备完毕\033[0m"
 	echo -e "\033[32m3. 顺序读 测试\033[0m"
-	sysbench fileio --file-total-size=150G --file-test-mode=seqrd --file-num=20 --time=300 --max-requests=0 --threads=${process_num} run|grep "File operations:" -A 8
+	sysbench fileio --file-total-size=150G --file-test-mode=seqrd --file-num=20 --time=300 --max-requests=0 --threads=${process_num} --report-interval=30 run
 	echo  -e "\033[32m5. 随机读 测试\033[0m"
-	sysbench fileio --file-total-size=150G --file-test-mode=rndrd --file-num=20 --time=300 --max-requests=0 --threads=${process_num} run|grep "File operations:" -A 8
+	sysbench fileio --file-total-size=150G --file-test-mode=rndrd --file-num=20 --time=300 --max-requests=0 --threads=${process_num} --report-interval=30 run
 	echo -e "\033[32m2. 顺序写 测试\033[0m"
-	sysbench fileio --file-total-size=150G --file-test-mode=seqwr --file-num=20 --time=300 --max-requests=0 --threads=${process_num} run|grep "File operations:" -A 8
+	sysbench fileio --file-total-size=150G --file-test-mode=seqwr --file-num=20 --time=300 --max-requests=0 --threads=${process_num} --report-interval=30 run
 	echo  -e "\033[32m4. 随机写 测试\033[0m"
-	sysbench fileio --file-total-size=150G --file-test-mode=rndwr --file-num=20 --time=300 --max-requests=0 --threads=${process_num} run|grep "File operations:" -A 8
+	sysbench fileio --file-total-size=150G --file-test-mode=rndwr --file-num=20 --time=300 --max-requests=0 --threads=${process_num} --report-interval=30 run
 	echo -e "\033[32m清除测试数据\033[0m"
 	sysbench fileio cleanup
 
@@ -65,51 +65,110 @@ io_test(){
 mem_test(){
 	echo -e "\033[32m开始内存性能测试\033[0m"
 	echo -e "\033[32m内存 顺序写 测试\033[0m"
-	sysbench memory --memory-oper=write --memory-access-mode=seq --time=30 run|grep "Total operations:" -A 3
+	sysbench memory --memory-oper=write --memory-access-mode=seq --report-interval=10 --time=30 run
 	echo -e "\033[32m内存 顺序读 测试\033[0m"
-	sysbench memory --memory-oper=read --memory-access-mode=seq --time=30 run|grep "Total operations:" -A 3
+	sysbench memory --memory-oper=read --memory-access-mode=seq --report-interval=10 --time=30 run
 	echo -e "\033[32m内存 随机写 测试\033[0m"
-	sysbench memory --memory-oper=write --memory-access-mode=rnd --time=30 run|grep "Total operations:" -A 3
+	sysbench memory --memory-oper=write --memory-access-mode=rnd --report-interval=10  --time=30 run
 	echo -e "\033[32m内存 随机读 测试\033[0m"
-	sysbench memory --memory-oper=read --memory-access-mode=rnd --time=30 run|grep "Total operations:" -A 3
+	sysbench memory --memory-oper=read --memory-access-mode=rnd  --report-interval=10 --time=30 run
 }
 
 
 cpu_test(){
 	process_num=`cat /proc/cpuinfo |grep process|wc -l`
 	echo -e "\033[32m开始 CPU 性能测试 cpu线程数量: ${process_num}\033[0m"
-	sysbench cpu --cpu-max-prime=20000 --threads=${process_num} run |grep "CPU speed:" -A 6
+	sysbench cpu --cpu-max-prime=20000 --threads=${process_num} --report-interval=10 run 
 }
 
 mysql_test(){
 	clear
-	echo "数据库测试 确保数据库中存在sbtest库，且测试账号拥有权限。”
-	echo “请输入数据库连接信息"
-	base_dir='/usr/share/sysbench/'
-	read -p "数据库用户名:" username
-	read -sp "数据库密码:" passwd
-	echo ""
+	echo -ne "数据库测试 \033[31m确保数据库中存在sbtest库，且测试账号拥有权限。\033[0m"
 	while [ 1 -eq 1 ]
+	do
+		read -p "数据库准备是否OK ?[Y/N]" prepare
+		case "$prepare" in
+		      	"N")  
+				exit 0
+				;;
+			"Y")
+				break
+				;;
+	
+			*)
+				echo "输入错误！"
+				;;
+		esac	
+	done
+
+	base_dir='/usr/share/sysbench'
+	while [ 1 -eq 1 ]
+	do
+		echo "请输入数据库连接信息"
+		
+		while [ 1 -eq 1 ]
 		do
 			read -p "请选择mysql 连接方式[socket|host]:" connect
 			case "$connect" in
 			"socket")
 				read -p "请输入数据库socket文件路径:" connect_way
-				[ -S $connect_way ]&& connect_str="--mysql-host=${connect_way}";break || echo -e "\033[31msocket 文件路径不存在 或 不是一个socket文件！\033[0m"
+				[ -S $connect_way ]&& connect_str="--mysql-socket=${connect_way}";break || echo -e "\033[31msocket 文件路径不存在 或 不是一个socket文件！\033[0m"
 				;;
 			"host")
 				read -p "请输入数据库IP地址/域名:" connect_way
+				read -p "请输入数据库端口:" port
 				ping $connect_way -c 1 -w 5 >/dev/null 2>&1
-				[ $? -eq 0 ]&& connect_str="--mysql-host=${connect_way}";break || echo -e "\033[31m网络地址无法到达\033[0m"	
-			
+				[ $? -eq 0 ]&&{
+connect_str="--mysql-host=${connect_way} --mysql-port=${port}"
+break
+}|| echo -e "\033[31m网络地址无法到达\033[0m"	
+				;;
 			esac
-	
-	echo "数据库准备"
-	sysbench /usr/share/sysbench/oltp_read_only.lua --threads=4  --mysql-user=pycf --mysql-password=1qaz@WSXabc --mysql-port=3320 --tables=10 --table-size=1000000 prepare
- 
-	done
-	
+		done
 
+		read -p "数据库用户名:" username
+		read -sp "数据库密码:" passwd
+		echo ""
+		echo -e "\033[32m开始准备测试数据库\033[0m"
+		sysbench ${base_dir}/oltp_read_only.lua --threads=4  --mysql-user=${username} --mysql-password=${passwd} ${connect_str} --tables=10 --table-size=1000000 prepare
+	 	[ $? -eq 0 ]&&{
+echo -e "\033[32m测试准备完成\033[0m"
+break
+}||echo -e "\033[31m数据库准备失败\033[0m"
+		sleep 3 
+	done
+
+	while [ 1 -eq 1 ]
+	do
+	cat <<-EOF
+	请选择测试内容
+	1.数据库读性能测试
+	2.数据库读写性能测试
+	3.数据库写性能测试
+	4.退出
+	EOF
+	
+	echo -ne "\033[32m选择测试项目: \033[0m"
+	read select_num
+	case  "$select_num" in
+		"1")
+			sysbench /usr/share/sysbench/oltp_write_only.lua --threads=16 --events=0  --time=300  --mysql-user=${username} --mysql-password=${passwd} ${connect_str} --tables=10 --table-size=1000000 --range_selects=on --report-interval=10 run 
+			;;
+		"2")
+			sysbench /usr/share/sysbench/oltp_read_write.lua --threads=16 --events=0  --time=300  --mysql-user=${username} --mysql-password=${passwd} ${connect_str} --tables=10 --table-size=1000000 --range_selects=on --report-interval=10 run 
+			;;
+		"3")
+			sysbench /usr/share/sysbench/oltp_read_write.lua --threads=16 --events=0  --time=300  --mysql-user=${username} --mysql-password=${passwd} ${connect_str} --tables=10 --table-size=1000000 --range_selects=on --report-interval=10 run 
+			;;
+		"4")
+			sysbench ${base_dir}/oltp_read_only.lua --threads=4  --mysql-user=${username} --mysql-password=${passwd} ${connect_str} --tables=10 --table-size=1000000 cleanup
+			exit 0
+			;;
+		*)
+			echo -e "\033[31m请输入正确的测试项目编号\033[0m"
+			;;
+	esac
+	done
 
 }
 
